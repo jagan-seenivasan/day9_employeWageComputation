3bc2b940628bc8fae80dc52837426fbbe5475200
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-15 00:51:40 +0530

    uc5

--- UC5_CalculateEmpSalaryForMonth.cs
+++ UC5_CalculateEmpSalaryForMonth.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee_Wages_usingObject
+namespace Employee_Wages_Computation
 {
     internal class UC5_CalculateEmpSalaryForMonth
     {
