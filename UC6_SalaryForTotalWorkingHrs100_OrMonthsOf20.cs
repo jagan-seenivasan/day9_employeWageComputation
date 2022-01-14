5475ec84d832a317d6fc43cacf3d601528c97fc0
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-15 01:00:30 +0530

    uc6

--- UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20.cs
+++ UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee_Wages_usingObject
+namespace Employee_Wages_Computation
 {
      class UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20
     {
