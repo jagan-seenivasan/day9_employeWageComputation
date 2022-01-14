fe1361ec8fe84a3ab65ca8ca1c81b35a009ddd71
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-15 00:29:41 +0530

    uc8

---
+++ uc1_FromMultipleCompanies.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day9_EmployeeWageComputation
+{
+    interface UC8_MultipleCompany
+    {
+        public int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int MaxHoursPerMonth);
+    }
+}
